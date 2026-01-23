7d0cf02946340db46061859f983c977ce45b2578
jackrichter <jackrichter1@gmail.com>
2026-01-23 02:34:54 +0100

    Implementing the Save(POST) operation using JPA and testing.

--- src/test/java/guru/springframework/spring7restmvc/controller/BeerControllerIT.java
+++ src/test/java/guru/springframework/spring7restmvc/controller/BeerControllerIT.java
@@ -7,6 +7,9 @@ import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.Rollback;
 
 import java.util.List;
@@ -23,6 +26,27 @@ class BeerControllerIT {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Rollback
+    @Transactional
+    @Test
+    void testSaveNewBeer()
+    {
+        BeerDTO beerDto = BeerDTO.builder()
+                .beerName("New Beer")
+                .build();
+
+        ResponseEntity responseEntity = beerController.handlePost(beerDto);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(201));
+        assertThat(responseEntity.getHeaders().getLocation()).isNotNull();
+
+        String[] locationUUID = responseEntity.getHeaders().getLocation().getPath().split("/");
+        UUID savedUUID = UUID.fromString(locationUUID[4]);
+
+        Beer beer = beerRepository.findById(savedUUID).get();
+        assertThat(beer).isNotNull();
+    }
 
     @Test
     void testBeerIdNotFound() {
