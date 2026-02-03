d60183cfb187ba7c67a1b1fe133565a6c4f435c4
jackrichter <jackrichter1@gmail.com>
2026-02-03 05:49:02 +0100

    JPA error handler.

--- src/main/java/guru/springframework/spring7restmvc/controller/CustomErrorController.java
+++ src/main/java/guru/springframework/spring7restmvc/controller/CustomErrorController.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring7restmvc.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +13,11 @@ import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class CustomErrorController {
+
+    @ExceptionHandler
+    ResponseEntity handleJPAViolations(TransactionSystemException ex) {
+        return ResponseEntity.badRequest().build();
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     ResponseEntity handleBindError(MethodArgumentNotValidException ex) {
