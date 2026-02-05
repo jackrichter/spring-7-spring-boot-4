3fdab241d53e0bdecfb310069eb9826c81a019bb
jackrichter <jackrichter1@gmail.com>
2026-02-05 01:09:31 +0100

    Making Corrections needed for Spring Boot 4, in order for the localhostmysql profile to work. It has to do with @UuidGenerator and @JdbcTypeCode(SqlTypes.CHAR).

--- src/main/java/guru/springframework/spring7restmvc/entities/Customer.java
+++ src/main/java/guru/springframework/spring7restmvc/entities/Customer.java
@@ -2,7 +2,9 @@ package guru.springframework.spring7restmvc.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -19,6 +21,7 @@ public class Customer {
     @GeneratedValue(generator = "UUID")
     @UuidGenerator                      // Hibernate specific (new SpringBoot 3.4 or higher)
     @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    @JdbcTypeCode(SqlTypes.CHAR)        // Hibernate specific (new SpringBoot 3.4 or higher)
     private UUID id;
 
     private String name;
