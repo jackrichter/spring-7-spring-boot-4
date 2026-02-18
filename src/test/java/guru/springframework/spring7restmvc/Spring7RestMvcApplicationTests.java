37215cb624188f0d27ebc03a2e515a524af01278
jackrichter <jackrichter1@gmail.com>
2026-02-18 20:48:20 +0100

    Re-enabling docker-compose Maven dependency in conjunction with TestContainers.

--- src/test/java/guru/springframework/spring7restmvc/Spring7RestMvcApplicationTests.java
+++ src/test/java/guru/springframework/spring7restmvc/Spring7RestMvcApplicationTests.java
@@ -2,8 +2,10 @@ package guru.springframework.spring7restmvc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("localmysql")
 class Spring7RestMvcApplicationTests {
 
     @Test
